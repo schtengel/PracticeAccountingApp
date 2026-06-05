33a4b11091b469cd84d397de0db284712a781a3b
schtengel <katzilipsiz@gmail.com>
2026-06-05 13:04:14 +0500

    Добавьте файлы проекта.

---
+++ PracticeAccountingApp/Views/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace PracticeAccountingApp.Views
+{
+    public partial class MainWindow : Window
+    {
+        public string CurrentDate =>
+            DateTime.Now.ToString("dd.MM.yyyy");
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            DataContext = this;
+
+            MainFrame.Navigate(
+                new Uri("Pages/HomePage.xaml",
+                UriKind.Relative));
+
+            NavigationMenu.SelectedIndex = 0;
+        }
+
+        private void NavigationMenu_SelectionChanged(
+            object sender,
+            SelectionChangedEventArgs e)
+        {
+            if (NavigationMenu.SelectedItem is ListBoxItem item)
+            {
+                string page = item.Tag.ToString();
+
+                MainFrame.Navigate(
+                    new Uri(page,
+                    UriKind.Relative));
+            }
+        }
+    }
+}
+
